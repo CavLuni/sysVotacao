ac4287a0211164984a26221e933fd06eebcd38a5
CavLuni <denis.luniere@gmail.com>
2020-06-03 14:32:11 -0300

    Projeto inicial

---
+++ src/main/java/sys/SysvotacaoApplication.java
@@ -0,0 +1,13 @@
+package sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysvotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysvotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sys/SysvotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysvotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
